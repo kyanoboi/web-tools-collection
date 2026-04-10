0458dc404591c02995eb16ee153a02957687a982
xieqy <kyantse0922@gmail.com>
2026-04-10 15:22:26 +0800

    Add new resources to README.md

--- windows sync.bat
+++ windows sync.bat
@@ -1,15 +1,21 @@
 @echo off
 
-echo Git add...
+echo [1/3] Git add...
 git add .
 
-echo Git commit...
+echo [2/3] Git commit...
 git diff --cached --quiet && (
-  echo No changes to commit
+  echo No new changes to commit
 ) || (
   git commit -m "Add new resources to README.md"
-  echo Git push...
-  git push
+)
+
+echo [3/3] Git push...
+:: 独立出来的 push 操作，并加上了简单的失败提示
+git push && (
+  echo Push successful!
+) || (
+  echo [ERROR] Push failed! Please check your network connection.
 )
 
 echo Done!
